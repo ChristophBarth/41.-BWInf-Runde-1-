5d505992499a361041774ed6884da82b0a30e17e
Christoph Barth <christoph.barth@t-online.de>
2022-11-21 16:14:44 +0100

    Implementierung von Tupeln in Aufgabe 2, Kommentieren der ersten beiden Aufgaben

--- 1 Störung/main.py
+++ 1 Störung/main.py
@@ -1,10 +1,9 @@
 import re
 
-word = "[A-Za-z',.!?ßäöü»«]+"
+word = "[A-Za-z',.!?ßäöü»«]+" #Regulärer Ausdruck für ein Wort variabler Länge mit Sonderzeichen
 
-alice_im_wunderland = open("res/Alice_im_Wunderland.txt", "r").read().lower()
-expression = open("res/stoerung1.txt", "r").read().replace("_", word)
-print(expression)
+alice_im_wunderland = open("res/Alice_im_Wunderland.txt", "r").read().lower() #Wort
+expression = open("res/stoerung1.txt", "r").read().replace("_", word) #Ausdruck - Erstetzen der fehlenden Wörter durch den regulären Ausdruck für Wort
 
-res = re.findall(expression, alice_im_wunderland)
-print([r for r in res])
+res = re.findall(expression, alice_im_wunderland) #Alle Matches von Epression in Wort finden
+print([r for r in res]) #Jedes Ergebnis ausgebem
--- 2 Verzinkt/main.py
+++ 2 Verzinkt/main.py
@@ -3,13 +3,14 @@ import seaborn as sb
 import numpy as np
 import random as rd
 from matplotlib.colors import LinearSegmentedColormap
-import copy
 
 custom_cmap = LinearSegmentedColormap.from_list("custom", ["white", "black", "red"])
 
-WIDTH = 100
-HEIGHT = 100
-seed_num = 3
+#Parameter
+
+WIDTH = 600
+HEIGHT = 600
+seed_num = 20
 
 speed_min = 2
 speed_max = 3
@@ -20,18 +21,17 @@ n_vector = np.array([0, 1])
 
 lightness = 2.5
 
-
+#Farbwert proportional zum Winkel des Wachstunsvektors zur Normalen
 def get_color_value_from_growth_speed(x_pos, y_pos, x_neg, y_neg):
     growth_vector = np.array([x_pos - x_neg, y_pos - y_neg])
     growth_vector_length = np.sqrt(growth_vector[0] ** 2 + growth_vector[1] ** 2)
 
-    if growth_vector_length == 0: return 1/lightness
-    # check for null vector in growth vector, as this would throw an error! no angle with null_vector possible
+    if growth_vector_length == 0: return 1/lightness #überprüfen, ob der Wachstumsvektor der Nullvektor ist, da sich für diesen Fall kein Winkel berechnen lässt
 
     angle = np.arccos((np.dot(n_vector, growth_vector)) / (1 * growth_vector_length))
-    return angle / (lightness*np.pi)
+    return angle / (lightness*np.pi) #Aufhellen des Bildes
 
-
+#Klasse der Kristallisationskeime
 class Seed:
     def __init__(self):
         self.x_pos_speed = rd.randint(speed_min, speed_max)
@@ -43,19 +43,19 @@ class Seed:
                                                              self.y_neg_speed)
         self.reproduce_in_frame = 1
 
-
+#Tupel aus Bildmatrix in Farbwerte umwandeln
 def prepare_for_plotting(img):
     for y in range(0, len(img)):
         for x in range(0, len(img[y])):
-            if isinstance(img[x][y], Seed):
-                if img[x][y].reproduce_in_frame == 1:
+            if isinstance(img[x][y][1], Seed):
+                if img[x][y][1] == 1:
                     img[x][y] = 2
                 else:
-                    img[x][y] = img[x][y].color_value
+                    img[x][y] = img[x][y][1].color_value
 
     return img
 
-
+#Bild darstellen
 def plot_image(img):
     img = prepare_for_plotting(img)
 
@@ -63,42 +63,42 @@ def plot_image(img):
 
     plt.show()
 
-
+#Berechnen des nächsten Bildes
 def generate_next_frame(img, frame):
     for y in range(0, len(img)):
-        for x in range(0, len(img[y])):
-            if isinstance(img[x][y], Seed) and img[x][y].reproduce_in_frame == frame:
-                current_seed = img[x][y]
+        for x in range(0, len(img[y])): #Für jeden Pixel
+            if isinstance(img[x][y][1], Seed) and img[x][y][0] == frame: #Wenn dieser ein Kristallisationskeim ist, der sich in diesem Frame reproduziert
 
+                #Reproduktion eines Kristallisationskeims:
+                current_seed = img[x][y][1]
                 for dx in range(-current_seed.x_neg_speed, current_seed.x_pos_speed + 1):
-                    if x + dx in range(0, WIDTH) and not isinstance(img[x + dx][y], Seed):
-                        img[x + dx][y] = copy.copy(current_seed)
-                        img[x + dx][y].reproduce_in_frame = frame + 1
+                    if x + dx in range(0, WIDTH) and not isinstance(img[x + dx][y][1], Seed):
+                        img[x + dx][y][1] = (current_seed)
+                        img[x + dx][y][0] = frame + 1
 
                 for dy in range(-current_seed.y_neg_speed, current_seed.y_pos_speed + 1):
-                    if y + dy in range(0, HEIGHT) and not isinstance(img[x][y + dy], Seed):
-                            img[x][y + dy] = copy.copy(current_seed)
-                            img[x][y + dy].reproduce_in_frame = frame + 1
-            elif rd.randint(0,spawn_prob) == 0:
-                s = Seed()
-                img[x][y] = s
-                img[x][y].reproduce_in_frame = frame+1
+                    if y + dy in range(0, HEIGHT) and not isinstance(img[x][y + dy][1], Seed):
+                            img[x][y + dy][1] = (current_seed)
+                            img[x][y + dy][0] = frame + 1
+
+            elif rd.randint(0,spawn_prob) == 0: #Wenn der betrachtete Bildpunkt kein Kristallisationkeim ist, kann dort nun einer erscheinen
+                img[x][y][1] = Seed()
+                img[x][y][0] = frame+1 #Reproduktion im nächsten Frame
     return img
 
 
-#TODO: Mit der Zeit neue Seeds einfügen -> beim Reproduzieren 1% Chanche, dass neuer Kristall entsteht
-#TODO: Copy.copy Methode ineffizient -> img als Reihungen aus Tupeln (frame, seed)
+#leere Bildmatrix generieren
+img = [[[0, 0] for i in range(WIDTH)] for j in range(HEIGHT)]
 
-
-img = [[0 for i in range(WIDTH)] for j in range(HEIGHT)]
-
+#Füllen der Matrix mit Kristallisationskeimen
 for i in range(0,seed_num):
-    img[rd.randint(1,WIDTH-1)][rd.randint(1,HEIGHT-1)] = Seed()
+    img[rd.randint(1,WIDTH-1)][rd.randint(1,HEIGHT-1)][1] = Seed()
 
 for frame in range(1, 100):
     img = generate_next_frame(img, frame)
 
 
+#Solange neue Frames generieren, bis das neue Frame dem alten gleicht
 new_img = img
 old_img = 0
 frame = 0
@@ -107,5 +107,4 @@ while not old_img == new_img:
     old_img = new_img
     new_img = generate_next_frame(old_img, frame)
 
-plot_image(new_img)
-
+plot_image(new_img) #Darstellen des erzeugten Bildes
