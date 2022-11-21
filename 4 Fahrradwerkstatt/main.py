596fecf6ce2ef084658c832836b6c325e7602caa
Christoph Barth <christoph.barth@t-online.de>
2022-11-21 01:25:27 +0100

    Added Relative Delay as Metric

--- 4 Fahrradwerkstatt/main.py
+++ 4 Fahrradwerkstatt/main.py
@@ -12,22 +12,22 @@ def get_df(n):
     df['begin'] = df['recieved']
     df['completed'] = 0
     df['delay'] = 0
+    df['relative_delay'] = 0
 
     return df
-
-def get_valid_begin(t):
-    task_time = t%MINUTES_PER_DAY
-    if(task_time in OPENING_HOURS):return t
-    else:
-        if(task_time < OPENING_TIME):return t + (OPENING_TIME - task_time)
-        elif(task_time >= CLOSING_TIME):
-            return get_valid_begin(t+(MINUTES_PER_DAY-task_time+1))
 
 def get_closing_time_of_day(t):
     return ((t//MINUTES_PER_DAY)*MINUTES_PER_DAY)+CLOSING_TIME
 
 def get_opening_time_of_next_day(t):
     return (((t//MINUTES_PER_DAY)+1)*MINUTES_PER_DAY)+OPENING_TIME
+
+def get_valid_begin(t):
+    task_time = t%MINUTES_PER_DAY
+    if(task_time in OPENING_HOURS):return t
+    else:
+        if(task_time < OPENING_TIME):return t + (OPENING_TIME - task_time)
+        elif(task_time >= CLOSING_TIME):return get_opening_time_of_next_day(t)
 
 def get_time_til_closing(t):
     return get_closing_time_of_day(t)-t
@@ -42,12 +42,13 @@ def get_completion_time(begin, time_remaining):
         return get_completion_time(begin, time_remaining)
 
 def evaluate_df(df):
-    print(df['delay'].describe())
+    metric = 'relative_delay'
+    print(df[metric].describe())
 
 def plot_hist(df1, df2):
     fig, axes = plt.subplots(1, 2)
-    df1['delay'].hist(bins=100, ax=axes[0])
-    df2['delay'].hist(bins=100, ax=axes[1])
+    df1['relative_delay'].hist(bins=100, ax=axes[0])
+    df2['relative_delay'].hist(bins=100, ax=axes[1])
     plt.show()
 
 def simulate_method1(df):
@@ -61,6 +62,7 @@ def simulate_method1(df):
                 df['begin'][i+1] = df['completed'][i]
 
     df['delay'] = df['completed']-df['recieved']
+    df['relative_delay'] = df['delay']/df['duration']
     evaluate_df(df)
     return df
 
@@ -78,10 +80,11 @@ def simulate_method2(df):
         df[i+1: i + counter] = df[i+1: i + counter].sort_values(by='duration')
 
     df['delay'] = df['completed']-df['recieved']
+    df['relative_delay'] = df['delay']/df['duration']
     evaluate_df(df)
     return df
 
-df = get_df(1)
+df = get_df(4)
 df1 = simulate_method1(df.copy())
 df2 = simulate_method2(df.copy())
 
